091599ebace9e1a253c5bbe6a2a7a4be5dcbed50
vasamsheridan <vasam@sheridancollege.ca>
2023-05-18 16:08:27 -0700

    Student details version 1

---
+++ src/studentdetailsprint/StudentDetailsPrint.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentdetailsprint;
+
+import java.util.Scanner;
+
+/** Creating Local Repository in Laptop; 
+ * initilize versioning git
+ * .gitconfig file
+ * .gitignore file
+ * commit
+ * 
+ *
+ * @author srinivsi
+ */
+public class StudentDetailsPrint {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Student[] list = new Student [3];
+        Scanner in =  new Scanner(System.in);
+        
+        for (int i = 0; i<list.length; i++) 
+        {
+            String n =in.next();
+            int a = in.nextInt();
+            
+        }
+    }
+    
+}
